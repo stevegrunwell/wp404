1368fe1d18ce613d368c3df28297df1d57023f00
Steve Grunwell <stevegrunwell@gmail.com>
2016-03-03 17:17:37 -0500

    Add Request\post_exists() to get data about unpublished or otherwise hidden posts

--- inc/request.php
+++ inc/request.php
@@ -20,3 +20,36 @@ function server_superglobal( $report ) {
 	) );
 }
 add_filter( 'wp404_report_data', __NAMESPACE__ . '\server_superglobal' );
+
+/**
+ * Try to determine if we have a post ID and, if so, get data directly from the database (bypassing
+ * any sort of cache) to get that post data.
+ *
+ * @global $wpdb
+ *
+ * @param array $report The WP404 report array.
+ * @param WP_Query $wp_query The WP_Query object that determined the 404 status.
+ * @return The filtered $report array.
+ */
+function post_exists( $report, $wp_query ) {
+	global $wpdb;
+
+	$post = array();
+
+	// WP_Query found something, but the user can't see it.
+	if ( 0 < $wp_query->found_posts ) {
+		$post = $wpdb->get_row( $wp_query->request );
+
+	// We have a post ID in the query string.
+	} elseif ( $post_id = get_query_var( 'p', false ) ) {
+		$post = $wpdb->get_row( $wpdb->prepare(
+			"SELECT * FROM $wpdb->posts WHERE ID = %d LIMIT 1",
+			$post_id
+		) );
+	}
+
+	return array_merge( $report, array(
+		'post_data' => $post,
+	) );
+}
+add_filter( 'wp404_report_data', __NAMESPACE__ . '\post_exists', 10, 2 );
--- tests/PHPUnit/RequestTest.php
+++ tests/PHPUnit/RequestTest.php
@@ -9,6 +9,7 @@
 namespace WP404;
 
 use WP_Mock as M;
+use Mockery;
 
 class RequestTest extends TestCase {
 
@@ -20,6 +21,83 @@ class RequestTest extends TestCase {
 		$this->assertEquals( array(
 			'$_SERVER' => $_SERVER,
 		), Request\server_superglobal( array() ) );
+	}
+
+	public function test_post_exists_with_found_posts() {
+		global $wpdb;
+
+		$wpdb = Mockery::mock( '\WP_Query' )->makePartial();
+		$wpdb->shouldReceive( 'get_row' )
+			->once()
+			->with( 'REQUEST_SQL' )
+			->andReturn( array(
+				'ID'         => 17,
+				'post_title' => 'Hello World',
+			) );
+
+		$wp_query = new \stdClass;
+		$wp_query->found_posts = 1;
+		$wp_query->request     = 'REQUEST_SQL';
+
+		$this->assertEquals( array(
+			'post_data' => array(
+				'ID'         => 17,
+				'post_title' => 'Hello World',
+			),
+		), Request\post_exists( array(), $wp_query ) );
+
+		$wpdb = null;
+	}
+
+	public function test_post_exists_with_p_argument() {
+		global $wpdb;
+
+		M::wpFunction( 'get_query_var', array(
+			'times'  => 1,
+			'args'   => array( 'p', false ),
+			'return' => 17,
+		) );
+
+		$wpdb = Mockery::mock( '\wpdb' );
+		$wpdb->posts = 'TABLE';
+		$wpdb->shouldReceive( 'prepare' )
+			->once()
+			->with( 'SELECT * FROM TABLE WHERE ID = %d LIMIT 1', 17 )
+			->andReturn( 'PREPARED' );
+		$wpdb->shouldReceive( 'get_row' )
+			->once()
+			->with( 'PREPARED' )
+			->andReturn( array(
+				'ID'         => 17,
+				'post_title' => 'Hello World',
+			) );
+
+		$wp_query = new \stdClass;
+		$wp_query->found_posts = 0;
+
+		$this->assertEquals( array(
+			'post_data' => array(
+				'ID'         => 17,
+				'post_title' => 'Hello World',
+			),
+		), Request\post_exists( array(), $wp_query ) );
+
+		$wpdb = null;
+	}
+
+	public function test_post_exists_with_no_p_argument() {
+		$wp_query = new \stdClass;
+		$wp_query->found_posts = 0;
+
+		M::wpFunction( 'get_query_var', array(
+			'times'  => 1,
+			'args'   => array( 'p', false ),
+			'return' => false,
+		) );
+
+		$this->assertEquals( array(
+			'post_data' => array(),
+		), Request\post_exists( array(), $wp_query ) );
 	}
 
 }
